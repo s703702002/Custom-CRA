6b9a18afd4a9c82977b20b81d060c0eef728dd2e
stanley_Huang <s703702002@gmail.com>
2020-04-12 02:18:33 +0800

    Custom eslintrc create react app template

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:prettier/recommended",
+    "prettier/react"
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    "react-hooks"
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-underscore-dangle": "off",
+    "global-require": "off"
+  },
+};
